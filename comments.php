3998e11d5f758ec01e968b0b10101ae887db36e4
sussan0416 <sussan.0416+github@gmail.com>
2018-01-22 18:35:37 +0900

    first commit, step 3-2

---
+++ comments.php
@@ -0,0 +1,2 @@
+<?php comment_form(); ?>
+<p>トラックバックURL: <?php trackback_url(); ?></p>
---
+++ single.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>ページタイトル</title>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+    <link href="http://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+</head>
+<body>
+
+<?php if(have_posts()):
+while(have_posts()): the_post(); ?>
+
+<article <?php post_class(); ?>>
+
+<h1><?php the_title(); ?></h1>
+
+<div class="postinfo">
+<time datetime="<?php echo get_the_date( 'c' ); ?>">
+<?php echo get_the_date(); ?>
+</time>
+</div>
+
+<?php the_content(); ?>
+
+<div class="navlink">
+<span class="navlink-prev">
+<?php previous_post_link('%link', '<i class="icon-chevron-sign-left"></i> %title'); ?>
+</span>
+
+<span class="navlink-next">
+<?php next_post_link('%link', '%title <i class="icon-chevron-sign-right"></i>'); ?>
+</span>
+</div>
+
+<?php comments_template(); ?>
+
+</artcle>
+<?php endwhile; endif; ?>
+
+</body>
+</html>
