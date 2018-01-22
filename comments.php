f37385cb934dc54835367d6c9422229364d2de46
sussan0416 <sussan.0416+github@gmail.com>
2018-01-22 21:01:51 +0900

    コメントの表示 step3-3

--- comments.php
+++ comments.php
@@ -1,4 +1,16 @@
-<?php comment_form([
+<?php if ( have_comments() ) : ?>
+<div id="comments">
+<h3>Comments</h3>
+<ul>
+<?php wp_list_comments([
+    'format' => 'html5'
+]); ?>
+</ul>
+</div>
+<?php endif; ?>
+
+<?php
+comment_form([
     'title_reply' => 'Leave a Comment',
     'label_submit' => '送信',
     'format' => 'html5'
