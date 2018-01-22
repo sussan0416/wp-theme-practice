b9308627601c7940f87603f6c4291a45b3cf5768
sussan0416 <sussan.0416+github@gmail.com>
2018-01-22 20:34:03 +0900

    フォームのデザイン変更

--- comments.php
+++ comments.php
@@ -1,2 +1,6 @@
-<?php comment_form(); ?>
+<?php comment_form([
+    'title_reply' => 'Leave a Comment',
+    'label_submit' => '送信',
+    'format' => 'html5'
+    ]); ?>
 <p>トラックバックURL: <?php trackback_url(); ?></p>
