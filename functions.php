e58fcde1c2b7ff80dc09166f010efe9d41ab9c91
sussan0416 <sussan.0416+github@gmail.com>
2018-01-23 22:23:09 +0900

    comment-replyをenqueueするように設定

--- functions.php
+++ functions.php
@@ -3,3 +3,9 @@
 if ( !isset( $content_width ) ) {
     $content_width = 840;
 }
+
+// デフォルトスクリプト
+function my_scripts() {
+    wp_enqueue_script( 'comment-reply' );
+}
+add_action( 'wp_enqueue_scripts', 'my_scripts' );
