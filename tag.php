700470a1a2c12a806a5e032d004bdce999698fdc
sussan0416 <sussan.0416+github@gmail.com>
2018-02-11 22:23:15 +0900

    タグページを追加

---
+++ tag.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>ページタイトル</title>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+    <link href="http://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+<?php wp_head(); ?>
+</head>
+<body>
+
+<section class="list">
+<h1 class="list-title"><span>TAG</span>
+<?php echo single_tag_title(); ?></h1>
+
+<?php echo tag_description(); ?>
+
+<?php if(have_posts()):
+while(have_posts()): the_post(); ?>
+
+<article <?php post_class(); ?>>
+
+<a href="<?php the_permalink(); ?>">
+
+<?php if ( has_post_thumbnail() ): ?>
+    <?php the_post_thumbnail( 'thumbnail' ); ?>
+<?php else: ?>
+    <?php preg_match( '/wp-image-(\d+)/s', $post->post_content, $thumb ); ?>
+    <?php if ($thumb): ?>
+        <?php echo wp_get_attachment_image( $thumb[1], 'thumbnail' ); ?>
+    <?php else: ?>
+        <img src="<?php echo get_template_directory_uri(); ?>/thumb.jpg" alt=""> 
+    <?php endif; ?>
+<?php endif; ?>
+
+<h1><?php the_title(); ?></h1>
+
+<div class="postinfo">
+<time datetime="<?php echo get_the_date( 'c' ); ?>">
+<?php echo get_the_date(); ?>
+</time>
+</div>
+
+<?php the_excerpt(); ?>
+
+</a>
+</article>
+
+<?php endwhile; endif; ?>
+
+<?php if ($wp_query->max_num_pages > 1): ?>
+<div class="navlink">
+<span class="navlink-prev">
+<?php next_posts_link('<i class="icon-chevron-sign-left"></i> 古い記事 '); ?>
+</span>
+<span class="navlink-next">
+<?php previous_posts_link(' 新しい記事 <i class="icon-chevron-sign-right"></i>'); ?>
+</span>
+</div>
+<?php endif; ?>
+
+</section>
+
+<?php wp_footer(); ?>
+</body>
+</html>
