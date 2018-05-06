271964ae5c250a55e37a015114f2b640cb345dfa
sussan0416 <sussan.0416+github@gmail.com>
2018-05-06 22:50:14 +0900

    ウィジェットにスタイルを適用

--- sidebar.php
+++ sidebar.php
@@ -1,14 +1,14 @@
 <div class="columns">
 
-<div class"col">
+<div class="col">
 <?php dynamic_sidebar( 'column01' ); ?>
 </div>
 
-<div class"col">
+<div class="col">
 <?php dynamic_sidebar( 'column02' ); ?>
 </div>
 
-<div class"col col03">
+<div class="col col03">
 <?php dynamic_sidebar( 'column03' ); ?>
 </div>
 
